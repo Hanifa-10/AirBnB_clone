b3535b8b3bbc2adb34ecc5a38ce4f7f017fe88bc
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-11-17 12:31:56 +0300

    Update __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+creating a unique FileStorage instance
+for the application.
+"""
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
+
+
+classes = {"BaseModel": BaseModel, "User": User, "City": City,
+           "Place": Place, "Review": Review, "Amenity": Amenity,
+           "State": State}
+
+storage = FileStorage()
+storage.reload()
